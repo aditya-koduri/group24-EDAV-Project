3e6d432660f014a027833806b9becc94eb15cd12
smarthxg <smarthgupta1998@gmail.com>
2021-04-05 19:48:54 +0530

    adds complaits js

---
+++ docs/complaints.js
@@ -0,0 +1,310 @@
+const complaint = [
+ {
+   "precinct": 1,
+   "count": 174
+ },
+ {
+   "precinct": 5,
+   "count": 160
+ },
+ {
+   "precinct": 6,
+   "count": 175
+ },
+ {
+   "precinct": 7,
+   "count": 221
+ },
+ {
+   "precinct": 9,
+   "count": 266
+ },
+ {
+   "precinct": 10,
+   "count": 241
+ },
+ {
+   "precinct": 13,
+   "count": 152
+ },
+ {
+   "precinct": 14,
+   "count": 351
+ },
+ {
+   "precinct": 17,
+   "count": 76
+ },
+ {
+   "precinct": 18,
+   "count": 268
+ },
+ {
+   "precinct": 19,
+   "count": 167
+ },
+ {
+   "precinct": 20,
+   "count": 112
+ },
+ {
+   "precinct": 22,
+   "count": 12
+ },
+ {
+   "precinct": 23,
+   "count": 610
+ },
+ {
+   "precinct": 24,
+   "count": 205
+ },
+ {
+   "precinct": 25,
+   "count": 614
+ },
+ {
+   "precinct": 26,
+   "count": 158
+ },
+ {
+   "precinct": 28,
+   "count": 344
+ },
+ {
+   "precinct": 30,
+   "count": 328
+ },
+ {
+   "precinct": 32,
+   "count": 684
+ },
+ {
+   "precinct": 33,
+   "count": 272
+ },
+ {
+   "precinct": 34,
+   "count": 400
+ },
+ {
+   "precinct": 40,
+   "count": 1009
+ },
+ {
+   "precinct": 41,
+   "count": 512
+ },
+ {
+   "precinct": 42,
+   "count": 653
+ },
+ {
+   "precinct": 43,
+   "count": 772
+ },
+ {
+   "precinct": 44,
+   "count": 1139
+ },
+ {
+   "precinct": 45,
+   "count": 223
+ },
+ {
+   "precinct": 46,
+   "count": 1120
+ },
+ {
+   "precinct": 47,
+   "count": 944
+ },
+ {
+   "precinct": 48,
+   "count": 662
+ },
+ {
+   "precinct": 49,
+   "count": 321
+ },
+ {
+   "precinct": 50,
+   "count": 215
+ },
+ {
+   "precinct": 52,
+   "count": 759
+ },
+ {
+   "precinct": 60,
+   "count": 485
+ },
+ {
+   "precinct": 61,
+   "count": 214
+ },
+ {
+   "precinct": 62,
+   "count": 176
+ },
+ {
+   "precinct": 63,
+   "count": 274
+ },
+ {
+   "precinct": 66,
+   "count": 89
+ },
+ {
+   "precinct": 67,
+   "count": 1119
+ },
+ {
+   "precinct": 68,
+   "count": 227
+ },
+ {
+   "precinct": 69,
+   "count": 488
+ },
+ {
+   "precinct": 70,
+   "count": 713
+ },
+ {
+   "precinct": 71,
+   "count": 648
+ },
+ {
+   "precinct": 72,
+   "count": 251
+ },
+ {
+   "precinct": 73,
+   "count": 1163
+ },
+ {
+   "precinct": 75,
+   "count": 2172
+ },
+ {
+   "precinct": 76,
+   "count": 158
+ },
+ {
+   "precinct": 77,
+   "count": 950
+ },
+ {
+   "precinct": 78,
+   "count": 118
+ },
+ {
+   "precinct": 79,
+   "count": 1062
+ },
+ {
+   "precinct": 81,
+   "count": 806
+ },
+ {
+   "precinct": 83,
+   "count": 648
+ },
+ {
+   "precinct": 84,
+   "count": 232
+ },
+ {
+   "precinct": 88,
+   "count": 229
+ },
+ {
+   "precinct": 90,
+   "count": 420
+ },
+ {
+   "precinct": 94,
+   "count": 113
+ },
+ {
+   "precinct": 100,
+   "count": 119
+ },
+ {
+   "precinct": 101,
+   "count": 595
+ },
+ {
+   "precinct": 102,
+   "count": 239
+ },
+ {
+   "precinct": 103,
+   "count": 572
+ },
+ {
+   "precinct": 104,
+   "count": 220
+ },
+ {
+   "precinct": 105,
+   "count": 470
+ },
+ {
+   "precinct": 106,
+   "count": 205
+ },
+ {
+   "precinct": 107,
+   "count": 168
+ },
+ {
+   "precinct": 108,
+   "count": 131
+ },
+ {
+   "precinct": 109,
+   "count": 205
+ },
+ {
+   "precinct": 110,
+   "count": 267
+ },
+ {
+   "precinct": 111,
+   "count": 37
+ },
+ {
+   "precinct": 112,
+   "count": 119
+ },
+ {
+   "precinct": 113,
+   "count": 704
+ },
+ {
+   "precinct": 114,
+   "count": 380
+ },
+ {
+   "precinct": 115,
+   "count": 245
+ },
+ {
+   "precinct": 120,
+   "count": 844
+ },
+ {
+   "precinct": 121,
+   "count": 366
+ },
+ {
+   "precinct": 122,
+   "count": 238
+ },
+ {
+   "precinct": 123,
+   "count": 112
+ }
+]
